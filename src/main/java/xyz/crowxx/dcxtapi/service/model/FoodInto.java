988592cdce6efdfd65fffd542ae16109257be080
Xx1871339825 <55568976+Xx1871339825@users.noreply.github.com>
2019-12-04 15:39:39 +0800

    前端api完成

---
+++ src/main/java/xyz/crowxx/dcxtapi/service/model/FoodInto.java
@@ -0,0 +1,38 @@
+package xyz.crowxx.dcxtapi.service.model;
+
+public class FoodInto {
+
+    /**
+     * name : 嘻嘻3
+     * price : 12
+     * num : 2
+     */
+
+    private String name;
+    private int price;
+    private int num;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
